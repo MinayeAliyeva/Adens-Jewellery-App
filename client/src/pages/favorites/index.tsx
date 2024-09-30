5d64265c484b71e5e2f3a0ae14c6877258b7211e
MinayeAliyeva <7s346pu@code.edu.az>
2024-09-30 11:10:43 +0400

    feature(feature/favorite-page):Favorite page created

--- client/src/pages/favorites/index.tsx
+++ client/src/pages/favorites/index.tsx
@@ -1,10 +1,12 @@
-import ProductCard from "./ProductCard";
+import { Avatar, Layout, List, Typography } from "antd";
+import FavoriteList from "./FavoriteList";
 const productImg1 = "/assets/images/product1.jpg";
 const productImg2 = "/assets/images/carusel3.jpg";
 const productImg3 = "/assets/images/carusel2.jpg";
+const backgroundFavorite = "/assets/images/bg-favorite.jpg";
 
-const Products = () => {
-  const products = [
+const Favorite = () => {
+  const favoriteProducts = [
     {
       productImg: productImg1,
       productImgHover: productImg2,
@@ -13,7 +15,6 @@ const Products = () => {
       productDiscountedPrice: "$15",
       productDetails: "Material: Cotton",
       productId: 1,
-      availableColors: ["#FF5733", "#33FF57", "#3357FF"],
       stock: 10,
       discount: 25,
     },
@@ -25,7 +26,6 @@ const Products = () => {
       productDiscountedPrice: "$20",
       productDetails: "Material: Silk",
       productId: 2,
-      availableColors: ["#FF33A1", "#3373FF", "#FFC300"],
       stock: 5,
       discount: 20,
     },
@@ -37,7 +37,6 @@ const Products = () => {
       productDiscountedPrice: "$25",
       productDetails: "Material: Wool",
       productId: 3,
-      availableColors: ["#FF5733", "#33FF57", "#3357FF"],
       stock: 0,
       discount: 16,
     },
@@ -49,7 +48,6 @@ const Products = () => {
       productDiscountedPrice: "$30",
       productDetails: "Material: Polyester",
       productId: 4,
-      availableColors: ["#FF33A1", "#3373FF", "#FFC300"],
       stock: 8,
       discount: 14,
     },
@@ -61,7 +59,6 @@ const Products = () => {
       productDiscountedPrice: "$32",
       productDetails: "Material: Leather",
       productId: 5,
-      availableColors: ["#FF5733", "#33FF57", "#3357FF"],
       stock: 2,
       discount: 20,
     },
@@ -73,22 +70,37 @@ const Products = () => {
       productDiscountedPrice: "$40",
       productDetails: "Material: Denim",
       productId: 6,
-      availableColors: ["#FF33A1", "#3373FF", "#FFC300"],
       stock: 0,
       discount: 11,
     },
   ];
-
   return (
     <>
-      <h1 className="text-center text-3xl font-bold my-8">Our Collection</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 p-5">
-        {products.map((product) => (
-          <ProductCard key={product.productId} product={product} />
-        ))}
-      </div>
+      <Layout
+        style={{
+          backgroundImage: `url(${backgroundFavorite})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "30vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography.Title
+          level={1}
+          style={{ color: "white", textAlign: "center" }}
+        >
+          Wishlist
+        </Typography.Title>
+      </Layout>
+      <Layout >
+        {favoriteProducts?.map((product) => {
+          return <FavoriteList product={product} />;
+        })}
+      </Layout>
     </>
   );
 };
 
-export default Products;
+export default Favorite;
