e7f1bbee7e42ae051ce3a1914e90d27df4bb67f1
MinayeAliyeva <7s346pu@code.edu.az>
2025-02-09 15:10:37 +0400

    style:deployment v-6 style

--- client/src/pages/home/Products.tsx
+++ client/src/pages/home/Products.tsx
@@ -9,23 +9,13 @@ const Products = () => {
 
   return (
     <Container
-      style={{
-        maxWidth: "2200px",
-        width: "100%",
-        margin: "0 auto",
-        textAlign: "center",
-        background: "red", 
-        padding: "10px", 
-        borderRadius: "10px", 
-        overflow: "hidden", 
-        flexWrap:"wrap"
-      }}
+   
     >
       <Typography className="text-3xl font-bold my-8 font-dancing-script text-center">
         OUR COLLECTION
       </Typography>
 
-      <Row gutter={[50, 50]} justify="center">
+      <Row gutter={[20, 40]} justify="center" style={{background:'red'}}>
         {data?.map?.((product: IProduct) => (
           <Col>
             <ProductCard product={product} />
