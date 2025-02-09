5c308f2834c1d23e8a7936c8fb38c205d02a4af1
MinayeAliyeva <7s346pu@code.edu.az>
2025-02-09 14:43:06 +0400

    style:deployment v-5 style

--- client/src/pages/home/Products.tsx
+++ client/src/pages/home/Products.tsx
@@ -1,9 +1,8 @@
 import { useGetProductsQuery } from "../../redux/api/product/product-api";
 import { IProduct } from "../../redux/api/product/models";
-import { Row, Col, Typography, Layout } from "antd";
+import { Row, Col, Typography } from "antd";
 import ProductCard from "../../shared/components/ProductCard";
 import Container from "../../shared/components/Container";
-const { Content } = Layout;
 
 const Products = () => {
   const { data } = useGetProductsQuery<{ data: IProduct[] }>("");
@@ -11,19 +10,24 @@ const Products = () => {
   return (
     <Container
       style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
+        maxWidth: "2200px",
+        width: "100%",
+        margin: "0 auto",
+        textAlign: "center",
+        background: "red", 
+        padding: "10px", 
+        borderRadius: "10px", 
+        overflow: "hidden", 
+        flexWrap:"wrap"
       }}
     >
       <Typography className="text-3xl font-bold my-8 font-dancing-script text-center">
         OUR COLLECTION
       </Typography>
 
-      <Row gutter={[50, 50]}  justify={"center"}>
+      <Row gutter={[50, 50]} justify="center">
         {data?.map?.((product: IProduct) => (
-          <Col key={product?._id}>
+          <Col>
             <ProductCard product={product} />
           </Col>
         ))}
